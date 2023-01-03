88f432bfc32d4ada84174a1fa4922965267dff2e
nicksales7 <nicksales72@gmail.com>
2023-01-03 01:04:05 -0500

    Solution to problem 70

---
+++ Problem70/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Problem70;
+
+public abstract class Program {
+    
+    public static void Main(string[] args)
+    {
+    }
+    
+    public int ClimbStairs(int n)
+    {
+        switch (n)
+        {
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            default:
+            {
+                var a = 1;
+                var b = 2;
+                for (var i = 2; i < n; i++)
+                {
+                    var c = a + b;
+                    a = b;
+                    b = c;
+                }
+                return b;
+            }
+        }
+    }
+}
