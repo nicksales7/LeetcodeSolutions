b386f4cebf0f0e8450d6b4c3170133236f7b7734
nicksales7 <nicksales72@gmail.com>
2023-01-03 01:06:03 -0500

    Solution to problem 9

---
+++ Problem9/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem9;
+
+public abstract class Program {
+    
+    public static void Main(string[] args)
+    {
+    }
+    
+    public bool IsPalindrome(int x)
+    {
+        var comparison = x;
+        var reversed = 0;
+
+        while (comparison != 0)
+        {
+            var digit = comparison % 10;
+            reversed = reversed * 10 + digit;
+
+            comparison /= 10;
+        }
+
+        if (x < 0)
+        {
+            return false;
+        }
+        else if (reversed == x)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
