1b6b0b2d527e9aac29f5747ef64875ef7134b4c1
nicksales7 <nicksales72@gmail.com>
2023-01-04 15:07:54 -0500

    Solution to problem 342

---
+++ Problem342/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Problem342;
+
+
+public abstract class Program {
+
+    public static void Main(string[] args)
+    {
+    }
+    
+    
+    public bool IsPowerOfTwo(int n)
+    {
+        var x = Convert.ToDouble(n);
+        double solution = Math.Log(x, 4);
+        return solution % 1 == 0;
+    }
+}
