bcc8503ec07c19e7976061120e12b4bd11233d92
nicksales7 <nicksales72@gmail.com>
2023-01-04 15:00:17 -0500

    Solution to problem 231

---
+++ Problem231/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Problem231;
+
+
+public class Program {
+
+    public static void Main(string[] args)
+    {
+    }
+    
+    
+    public bool IsPowerOfTwo(int n)
+    {
+        var x = Convert.ToDouble(n);
+        double solution = Math.Log2(x);
+        return solution % 1 == 0;
+    }
+}
---
+++ Problem66/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Problem66;
+
+public abstract class Program {
+    
+    public static void Main(string[] args)
+    {
+    }
+    
+    public int[] PlusOne(int[] digits)
+    {
+        digits = digits.Select(x => x + 1).ToArray();
+        return digits;
+
+    }
+    
+}
